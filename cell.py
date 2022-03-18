24a3e0f3414089f02333312ffde84505ff06ec87
Paul Jerricho <decaf2017@naver.com>
2022-03-18 18:22:12 +0900

    python1

--- cell.py
+++ cell.py
@@ -21,10 +21,11 @@ print(ws["A10"].value) # 값이 없을 땐 'NONE'을 출력
 # column = A,B,C,D
 print(ws.cell(row=1, column=1).value) #A1값이 출력
 print(ws.cell(row=1, column=2).value) #B1값이 출력
-
+index = 0
 for x in range(1,11): # 10개 row
     for y in range(1, 11): # 10개 column
-        ws.cell(row=x, column=y, value=randint(0,100)) # 0~100개 사이의 랜덤숫자
-
+        #ws.cell(row=x, column=y, value=randint(0,100)) # 0~100개 사이의 랜덤숫자
+        ws.cell(row=x,column=y,value=index) #1~100까지 자동으로 채워짐
+        index += 1
 wb.save("sample2.xlsx")
 
