4d6e812182ce6ce4af587286e1fc233eeb44a8ca
Paul Jerricho <decaf2017@naver.com>
2022-03-18 21:22:20 +0900

    RPA PROJECT

--- python/5_cell_range.py
+++ python/5_cell_range.py
@@ -1,5 +1,6 @@
 from curses import color_content
 import imp
+from tkinter.tix import COLUMN
 from openpyxl import Workbook
 from random import *
 wb = Workbook()
@@ -33,18 +34,38 @@ col_B = ws["B"] # 영어 column만 가지고 오기
 #         print(cell.value, end=" ")
 #     print()
 
-from openpyxl.utils.cell import coordinate_from_string
+# from openpyxl.utils.cell import coordinate_from_string
 
-row_range = ws[2:ws.max_row] #2번째 줄부터 마지막 줄까지
-for rows in row_range:
-    for cell in rows:
-        #print(cell.value, end=" ")
-        #print(cell.coordinate, end=" ") #셀 좌표 정보
-        xy = coordinate_from_string(cell.coordinate)
-        # print(xy, end=" ")
-        print(xy[0], end=" ")
-        print(xy[1], end=" ")
-    print()
+# row_range = ws[2:ws.max_row] #2번째 줄부터 마지막 줄까지
+# for rows in row_range:
+#     for cell in rows:
+#         #print(cell.value, end=" ")
+#         #print(cell.coordinate, end=" ") #셀 좌표 정보
+#         xy = coordinate_from_string(cell.coordinate)
+#         # print(xy, end=" ")
+#         print(xy[0], end=" ")
+#         print(xy[1], end=" ")
+#     print()
+
+# 전체 rows
+# print(tuple(ws.rows))
+# for row in tuple(ws.rows):
+#     print(row[1].value)
+
+# 전체 columns
+# print(tuple(ws.columns))
+# for column in tuple(ws.columns):
+#     print(column[0].value)
+
+# 2번째 줄부터 11번째 줄까지, 2번째 열부터 3번째 열까지
+# for row in ws.iter_rows(min_row=2, max_row=11, min_col=2, max_col=3): #전체 row
+#     #  print(row[0].value,row[1].value)
+#     print(row)
+
+for col in ws.iter_cols(min_row=1, max_row=5, min_col=1, max_col=3):
+    print(col)
+
+# for column in ws.iter_cols(): #전체 col
+#     print(column[2].value, end=" ")
 
 wb.save("sample3.xlsx")
-
