7977ef3b988d3711631955c871b4a861c4a730bd
Paul Jerricho <decaf2017@naver.com>
2022-03-28 14:54:30 +0900

    C#_MES

---
+++ C#_MES/C#/ReactArea/MainApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactArea
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 가로를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 세로를 입력하세요.");
+            string height = Console.ReadLine();
+            
+            Console.WriteLine("사각형의 넓이는?");
+            int result = int.Parse(width) * int.Parse(height); //string을 int로 바꿔줘야한다.
+            Console.WriteLine(result);
+        }
+    }
+}
