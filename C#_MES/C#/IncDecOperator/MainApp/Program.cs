f517c64999c11af877908bc9bc38776f06e53922
Paul Jerricho <decaf2017@naver.com>
2022-03-29 12:51:17 +0900

    C#_MES

---
+++ C#_MES/C#/IncDecOperator/MainApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncDecOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
