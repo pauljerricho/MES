27feabc6c792e7fa331a0cf632dd8aa179a34cb5
Paul Jerricho <decaf2017@naver.com>
2022-03-29 12:48:06 +0900

    C#_MES

---
+++ C#_MES/C#/ArithmaticOperators/MainApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmaticOperators
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 111 + 222;
+            Console.WriteLine($"a = {a}");
+
+            int b = a - 100;
+            Console.WriteLine($"b = {b}");
+
+            int c = b * 10;
+            Console.WriteLine($"c = {c}");
+
+            double d = a / 6.3;
+            Console.WriteLine($"d = {d}");
+
+            Console.WriteLine($"22 / 7 = {22 / 7}({22 & 7})");
+        }
+    }
+}
