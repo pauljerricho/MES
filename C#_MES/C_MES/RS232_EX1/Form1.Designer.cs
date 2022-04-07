905dcd095a9e09989aa70c3d2dc8da122a677fb1
Paul Jerricho <decaf2017@naver.com>
2022-04-07 18:38:09 +0900

    C#_MES_RS232

---
+++ C#_MES/C_MES/RS232_EX1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace RS232_EX1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CB_PORT = new System.Windows.Forms.ComboBox();
+            this.BT_Open = new System.Windows.Forms.Button();
+            this.BT_Close = new System.Windows.Forms.Button();
+            this.BT_Read = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TB_Sendframe = new System.Windows.Forms.TextBox();
+            this.TB_Receivedframe = new System.Windows.Forms.TextBox();
+            this.BT_P20 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(125, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(125, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "송신 프레임";
+            // 
+            // CB_PORT
+            // 
+            this.CB_PORT.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.CB_PORT.FormattingEnabled = true;
+            this.CB_PORT.Location = new System.Drawing.Point(216, 84);
+            this.CB_PORT.Name = "CB_PORT";
+            this.CB_PORT.Size = new System.Drawing.Size(158, 27);
+            this.CB_PORT.TabIndex = 1;
+            this.CB_PORT.Click += new System.EventHandler(this.CB_Port_Click);
+            // 
+            // BT_Open
+            // 
+            this.BT_Open.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BT_Open.Location = new System.Drawing.Point(129, 126);
+            this.BT_Open.Name = "BT_Open";
+            this.BT_Open.Size = new System.Drawing.Size(100, 40);
+            this.BT_Open.TabIndex = 2;
+            this.BT_Open.Text = "OPEN";
+            this.BT_Open.UseVisualStyleBackColor = true;
+            this.BT_Open.Click += new System.EventHandler(this.BT_Open_Click);
+            // 
+            // BT_Close
+            // 
+            this.BT_Close.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BT_Close.Location = new System.Drawing.Point(274, 126);
+            this.BT_Close.Name = "BT_Close";
+            this.BT_Close.Size = new System.Drawing.Size(100, 40);
+            this.BT_Close.TabIndex = 3;
+            this.BT_Close.Text = "CLOSE";
+            this.BT_Close.UseVisualStyleBackColor = true;
+            this.BT_Close.Click += new System.EventHandler(this.BT_Close_Click);
+            // 
+            // BT_Read
+            // 
+            this.BT_Read.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BT_Read.Location = new System.Drawing.Point(129, 176);
+            this.BT_Read.Name = "BT_Read";
+            this.BT_Read.Size = new System.Drawing.Size(100, 40);
+            this.BT_Read.TabIndex = 4;
+            this.BT_Read.Text = "읽기";
+            this.BT_Read.UseVisualStyleBackColor = true;
+            this.BT_Read.Click += new System.EventHandler(this.BT_Read_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(125, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "수신 프레임";
+            // 
+            // TB_Sendframe
+            // 
+            this.TB_Sendframe.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TB_Sendframe.Location = new System.Drawing.Point(216, 236);
+            this.TB_Sendframe.Name = "TB_Sendframe";
+            this.TB_Sendframe.Size = new System.Drawing.Size(158, 27);
+            this.TB_Sendframe.TabIndex = 6;
+            // 
+            // TB_Receivedframe
+            // 
+            this.TB_Receivedframe.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TB_Receivedframe.Location = new System.Drawing.Point(216, 271);
+            this.TB_Receivedframe.Name = "TB_Receivedframe";
+            this.TB_Receivedframe.Size = new System.Drawing.Size(158, 27);
+            this.TB_Receivedframe.TabIndex = 7;
+            // 
+            // BT_P20
+            // 
+            this.BT_P20.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BT_P20.Font = new System.Drawing.Font("함초롬바탕 확장", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BT_P20.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BT_P20.Location = new System.Drawing.Point(216, 322);
+            this.BT_P20.Name = "BT_P20";
+            this.BT_P20.Size = new System.Drawing.Size(100, 40);
+            this.BT_P20.TabIndex = 8;
+            this.BT_P20.Text = "P20";
+            this.BT_P20.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.BT_P20);
+            this.Controls.Add(this.TB_Receivedframe);
+            this.Controls.Add(this.TB_Sendframe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BT_Read);
+            this.Controls.Add(this.BT_Close);
+            this.Controls.Add(this.BT_Open);
+            this.Controls.Add(this.CB_PORT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CB_PORT;
+        private System.Windows.Forms.Button BT_Open;
+        private System.Windows.Forms.Button BT_Close;
+        private System.Windows.Forms.Button BT_Read;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TB_Sendframe;
+        private System.Windows.Forms.TextBox TB_Receivedframe;
+        private System.Windows.Forms.Button BT_P20;
+    }
+}
+
---
+++ C#_MES/C_MES/RS232_EX1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RS232_EX1
+{
+    public partial class Form1 : Form
+    {
+        //시리얼 포트 변수 my_serial 생성
+        SerialPort my_serial = new SerialPort();
+
+        //RS232 제어코드//
+        const char ETX = (char)0x03; //END TEXT [응답용 ASC]
+        const char EOT = (char)0x04; //END OF TEXT [요구용 ASC]
+        const char ENQ = (char)0x05; //ENQUIRE [프레임 시작코드]
+        const char ACK = (char)0x06; //ACK [응답 시작]
+        const char NAK = (char)0x15; //NOT ACK [응답  시작]
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormStart(true);
+        }
+
+        private void FormStart(bool t)
+        {
+            BT_Open.Enabled = t; //OPEN BT
+            BT_Close.Enabled = !t; //CLOSE BT
+            BT_Read.Enabled = !t; //READ BT
+            BT_P20.Enabled = !t; //P20 BUTTON(LAMP)
+            TB_Sendframe.Enabled = !t; //송신프레임 TEXT BOX
+            TB_Receivedframe.Enabled = !t; //수신프레임 TEXT BOX
+        }
+        private void CB_Port_Click(object sender, EventArgs e)
+        {
+            CB_PORT.Items.Clear();
+            foreach (string comport in SerialPort.GetPortNames())
+            {
+                CB_PORT.Items.Add(comport);
+            }
+        }
+
+        private void BT_Open_Click(object sender, EventArgs e)
+        {
+            my_serial.PortName = CB_PORT.Text;
+            my_serial.BaudRate = 19200;
+            my_serial.DataBits = 8;
+            my_serial.Parity = Parity.None;
+            my_serial.StopBits = StopBits.None;
+
+            if (!my_serial.IsOpen)
+            {
+                my_serial.Open();
+                FormStart(false);
+            }
+        }
+
+        private void BT_Close_Click(object sender, EventArgs e)
+        {
+            if (my_serial.IsOpen)
+            {
+                my_serial.Close();
+                FormStart(true);
+            }
+        }
+
+        private void BT_Read_Click(object sender, EventArgs e)
+        {
+            String SendDataText = ENQ + "OORRS" + "01" + "05%PX20" + EOT;
+            TB_Sendframe.Text = SendDataText;
+
+            SendData(SendDataText);
+        }
+
+        private void SendData(string sendDataText)
+        {
+            my_serial.Write(sendDataText);
+            do
+            { } while (my_serial.WriteBufferSize == 0);
+
+            Thread.Sleep(10);
+            DataRead();
+        }
+
+        private void DataRead()
+        {
+            bool m_Next = false;
+            string strlnData = string.Empty;
+            string RData = string.Empty;
+
+            do
+            {
+                string msg = my_serial.ReadExisting();
+                strlnData += msg;
+                if (msg.IndexOf(ETX) > 0)
+                {
+                    TB_Receivedframe.Text = strlnData;
+
+                    if (strlnData[0] == ACK)
+                    {
+                        RData += strlnData[11];
+                        if (Convert.ToInt16(RData) == 1)
+                            BT_P20.BackColor = Color.Green;
+                        else BT_P20.BackColor = Color.Gray;
+
+                        m_Next = true;
+                    }
+                    else if (strlnData[0] == NAK)
+                        m_Next = true;
+                }
+            } while (!m_Next);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
